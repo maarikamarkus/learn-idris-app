95f72745209221766e014991302ebb121b8a05a0
maarikamarkus <maarika.markus@gmail.com>
2022-02-12 19:11:55 +0200

    Add tailwind config and apply rules. Remove redundant styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  mode: 'jit',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  // content: ['apps/site/pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
