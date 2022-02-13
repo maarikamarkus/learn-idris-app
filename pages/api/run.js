25ab74af7a9f60ea6c64dd3d18531bbcfe2f0ae7
maarikamarkus <maarika.markus@gmail.com>
2022-02-13 15:36:53 +0200

    Add preliminary code to run user code in docker

--- pages/api/run.js
+++ pages/api/run.js
@@ -1,26 +1,38 @@
+
 // TODO: test if that works
 const fs = require('fs');
+const { exec } = require('child_process');
 
 export default function handler(req, res) {
   //console.log(req.body);
-
+  
   // sisendist kood
   const userCode = req.body.code;
   const lessonId = req.body.lessonId;
-
+  
   // juurde panna test case'id koos main funktsiooniga
   // kuidas tean, millisest test failist test case'id võtta? 
   // annan lessoni nime kaasa? ja loodan, et on sama nimega .yml _tests kaustas?
-
+  
   const testCases = getTestCases(lessonId);
-
-
-  // tekitada .idr fail
+  
+  
+  // tekitada .idr fail --> tglt koosta sõne muutujasse
   // TODO: kuidas js-iga faili kirjutada?
-
+  const testCode = userCode;
+  
   // jooksutada dockerit koos eelnevalt loodud .idr failiga
   // TODO: uuri dockerode vms, mis dockeriga suhelda aitab
+  const docker = exec('docker run -i idris', (err, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
 
+    res.status(200).send("hästi:)");
+  });
+  docker.stdin.write(testCode, () => {
+    docker.stdin.end();
+  });
+  
   // lugeda dockeri väljund
   // TODO: dockerode aitab?
 
@@ -51,5 +63,5 @@ function getTestCases(lessonId) {
 
 function parseTestCases(data) {
   // TODO: otsi mõni teek, millega yml parsida
-
+  return data;
 }
