dde73a1813d4c464515f3766e7eb0b8dcfde67e0
maarikamarkus <maarika.markus@gmail.com>
2022-02-27 15:48:00 +0200

    Check idris file compiling results before executing code

--- docker/evaluate.sh
+++ docker/evaluate.sh
@@ -5,6 +5,14 @@ file=test-test.idr
 # read user input + test code combination from stdin > $file
 cat - > $file
 
+# check idris source file
+idris2 $file --check
+
+if [ $? -eq 1 ]; then
+  rm $file
+  exit 1
+fi
+
 # run idris with tests
 idris2 $file --exec main
 
